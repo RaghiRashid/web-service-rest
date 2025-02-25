9afec19f7f7b296a8cb5cf2a638e291d0f524d20
Raghi Rashid <raghirashiddev@gmail.com>
2025-02-25 20:26:12 -0300

    first commit

---
+++ database/migrations/2025_02_25_223135_create_addresses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddressTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('street');
+            $table->string('number', 10);
+            $table->string('district');
+            $table->string('complement')->nullable();
+            $table->char('zip_code', 8);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('address');
+    }
+}
